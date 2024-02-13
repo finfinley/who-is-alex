f1a569e292afe6e78b1e528192863b94b8143d27
Alex Finley <alexfin@outlook.com>
2024-02-12 19:01:17 -0500

    blah

--- chronicler/src/app.module.ts
+++ chronicler/src/app.module.ts
@@ -7,7 +7,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://<USER>>:<PASSWORD>@chronicle.8ircsqe.mongodb.net/?retryWrites=true&w=majority',
+      'mongodb+srv://merlin:magic@chronicle.8ircsqe.mongodb.net/?retryWrites=true&w=majority',
     ),
     ArchivesModule,
   ],
