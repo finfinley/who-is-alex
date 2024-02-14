8c527a830928610f3f56154b2202eb6323862ced
Alex Finley <alexfin@outlook.com>
2024-02-13 19:14:15 -0500

    .env file

--- chronicler/src/app.module.ts
+++ chronicler/src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArchivesModule } from './archives/archives.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MongooseModule.forRoot(
-      'mongodb+srv://user:password@chronicle.8ircsqe.mongodb.net/chronicler?retryWrites=true&w=majority',
+      `mongodb+srv://${process.env.MONGOOSE_USER}:${process.env.MONGOOSE_PASSWORD}@chronicle.8ircsqe.mongodb.net/chronicler?retryWrites=true&w=majority`,
     ),
     ArchivesModule,
   ],
