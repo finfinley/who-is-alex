c489cc6a0207fe99d7d9d3a8b5ef573ea5ba0762
Alex Finley <alexfin@outlook.com>
2024-02-29 20:40:28 -0500

    tailwind configs

--- alex-finley/tailwind.config.ts
+++ alex-finley/tailwind.config.ts
@@ -1,27 +1,43 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
     colors: {
-      raisin: "#272838",
-      blue: "#5299D3",
-      tan: "#F2E9E4",
-      red: "#E05263",
-      violet: "#443850",
-      green: "#70A288",
-      black: "#353B3C"
+      raisin: '#272838',
+      blue: '#5299D3',
+      tan: '#F2E9E4',
+      red: '#E05263',
+      violet: '#443850',
+      green: '#70A288',
+      black: '#353B3C',
+    },
+    screens: {
+      sm: '375px',
+      // => @media (min-width: 640px) { ... }
+
+      md: '480px',
+      // => @media (min-width: 768px) { ... }
+
+      lg: '768px',
+      // => @media (min-width: 1024px) { ... }
+
+      xl: '1024px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
     },
   },
   plugins: [],
